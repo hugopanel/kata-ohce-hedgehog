4f301287cff1c5a0f5c0e3b15475744023a40ae0
loubruness <94389521+loubruness@users.noreply.github.com>
2025-01-15 13:47:28 +0100

    proposition tests greeters (both tests passed)

--- ts/tests/greeter.test.ts
+++ ts/tests/greeter.test.ts
@@ -1,12 +1,21 @@
 import { test, expect, jest } from "@jest/globals"
 import assert from 'node:assert'
 import Greeter from "../greeter"
-
+import {SystemClock} from "../greeter"
 
 test('should say "good night" at midnight', () => {
-  assert.fail("TODO")
+
+  const systemClock = new SystemClock() as jest.Mocked<SystemClock>;
+  systemClock.currentHour = jest.fn<() => number>().mockReturnValue(0);
+  const greeter = new Greeter();
+  greeter.clock = systemClock;
+  assert(greeter.greet() === 'Good night');
 })
 
 test('should never return undefined', () => {
-  assert.fail("TODO")
+  const systemClock = new SystemClock() as jest.Mocked<SystemClock>;
+  systemClock.currentHour = jest.fn<() => number>().mockReturnValue(25);
+  const greeter = new Greeter();
+  greeter.clock = systemClock;
+  assert(greeter.greet() != undefined);
 })
